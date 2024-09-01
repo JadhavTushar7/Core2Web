8814f9dd20768f0bbd2446b2dcb6a1450a088590
tushar jadhav <132814429+JadhavTushar7@users.noreply.github.com>
2024-09-01 10:31:36 +0530

    Add files via upload

---
+++ ScannerDemo2.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+class ScannerDmeo2 {
+    public static void main(String[] args) {
+
+            Scanner obj = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+  String name = obj.next();
+
+System.out.println("Enter your age: ");
+       
+	 int age = obj.nextInt();
+	 System.out.println(name);
+		 System.out.println (age);
+   }
+}
+
