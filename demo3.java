f5227e1f94e6d34b637ba7bd136d324743e8ebfa
tushar jadhav <132814429+JadhavTushar7@users.noreply.github.com>
2024-09-01 10:05:51 +0530

    Add files via upload

---
+++ demo3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class nameDemo1 {
+    public static void main(String[] args) {
+
+            Scanner obj = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+  String name = obj.next(); 
+        System.out.println(name);
+   }
+}
