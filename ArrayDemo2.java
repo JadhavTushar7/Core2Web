e78c56299039e4a7d3bf9cb0e0aad0cf11f43d82
tushar jadhav <132814429+JadhavTushar7@users.noreply.github.com>
2024-09-03 20:02:47 +0530

    Add files via upload

---
+++ ArrayDemo2.java
@@ -0,0 +1,43 @@
+class ArrayDemo2 {
+
+
+	public static void main(String[]args){
+	
+		
+	
+	      int arr1[] = {10,20,30,40,50};
+
+	      char arr2[] = {'a','b','c'};
+
+	      float arr3[] = {10.5f,20.5f};
+
+	      boolean arr4[] = { true, false, true};
+
+	    for (int i = 0; i < arr1.length; i++) {
+    System.out.println(arr1[i]);
+	 }
+
+ for (int i = 0; i < arr2.length; i++) {
+    System.out.println(arr2[i]);
+ }
+  for (int i = 0; i < arr3.length; i++) {
+    System.out.println(arr3[i]);
+  }
+   for (int i = 0; i < arr4.length; i++) {
+    System.out.println(arr4[i]);
+   }
+
+//for Specific Element
+
+	      //System.out.println(arr1[0]);
+	      //System.out.println(arr2[2]);
+	      //System.out.println(arr3[1]);
+	      //System.out.println(arr4[2]);
+	       
+	    
+      
+// find total num of Element
+  //  System.out.println("total element :" +arr1.length);
+
+	}
+}
