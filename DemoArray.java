f5e34a3aa807370d5e26432824ba1e8e249cc628
tushar jadhav <132814429+JadhavTushar7@users.noreply.github.com>
2024-09-03 14:16:33 +0530

    Add files via upload

---
+++ DemoArray.java
@@ -0,0 +1,18 @@
+class ArrayDemo {
+
+	public static  void main(String[] args) {
+	
+	int arr1[] = new int[4];	// int arr1[] = {0,1,2,3};
+
+			arr1[0] = 19;
+		        arr1[1] = 4;  
+			arr1[2] = 07;  
+			arr1[3] = 43;
+			 
+			  System.out.println(arr1[0]);
+			   System.out.println(arr1[1]);
+			    System.out.println(arr1[2]);
+			     System.out.println(arr1[3]);
+	
+	}
+}
