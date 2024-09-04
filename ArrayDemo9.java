ef3b342b05e844bf630bd536d221d04daec0eada
tushar jadhav <132814429+JadhavTushar7@users.noreply.github.com>
2024-09-04 19:46:00 +0530

    Add files via upload

---
+++ ArrayDemo9.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class ArrayDemo9{
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new  Scanner(System.in);
+
+		System.out.println("Enter Array size");
+		int size = sc.nextInt();
+
+		int arr[] = new int[size];
+		
+		System.out.println("Array sixe = " + arr.length);
+
+
+	
+	
+	}
+
+
+}
