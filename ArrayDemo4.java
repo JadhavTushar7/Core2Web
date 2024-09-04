09982d2efefa62cd637e8be37e96b47ca67721f8
tushar jadhav <132814429+JadhavTushar7@users.noreply.github.com>
2024-09-04 09:59:57 +0530

    Add files via upload

---
+++ ArrayDemo4.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class ArrayDemo4 {
+
+	public static void main(String[] args){
+	
+	
+		Scanner sc = new Scanner(System.in);
+
+		int arr[] = new int[5];
+
+		/* arr[0] = sc.nextInt();
+		 arr[1] = sc.nextInt();
+		  arr[2] = sc.nextInt();
+		   arr[3] = sc.nextInt();
+		    arr[5] = sc.nextInt();*/
+
+		    for(int x=0;x<5; x++){
+			    arr[x] = sc.nextInt();
+		    }
+for( int i = 0;i<5; i++){
+
+	System.out.println(arr[i]);
+}
+
+				    
+	}
+}
