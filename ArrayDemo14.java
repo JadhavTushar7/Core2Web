3eb928b6d30e98e0d7b8397dfe21714bf26f23ed
tushar jadhav <132814429+JadhavTushar7@users.noreply.github.com>
2024-09-06 16:02:31 +0530

    Add files via upload

---
+++ ArrayDemo14.java
@@ -0,0 +1,17 @@
+class ArrayDemo14{
+
+	public static void main(String[] args){
+	
+
+		int x =19;
+		int y = 4;
+		Integer z = 7;
+		  
+                     System.out.println(System.identityHashCode(x));
+		      System.out.println(System.identityHashCode(y));
+		       System.out.println(System.identityHashCode(z));
+	
+	
+	}
+
+}
