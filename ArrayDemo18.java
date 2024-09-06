99c3df122d94d267557cabd6e32da473a2f51032
tushar jadhav <132814429+JadhavTushar7@users.noreply.github.com>
2024-09-06 16:47:15 +0530

    Add files via upload

---
+++ ArrayDemo18.java
@@ -0,0 +1,14 @@
+class ArrayDemo18{
+  
+	public static void main(String[] args){
+
+
+                char arr5[] ={'V','T','J'} ;
+
+                  System.out.println(arr5);
+          
+	    String str = "tushar" ;
+              System.out.println(str);    
+
+        }
+}
