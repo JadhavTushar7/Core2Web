b904e8f2ed974cad66c1708d1a839e09420611da
tushar jadhav <132814429+JadhavTushar7@users.noreply.github.com>
2024-09-06 16:20:34 +0530

    Add files via upload

---
+++ ArrayDemo16.java
@@ -0,0 +1,22 @@
+class ArrayDemo16 {
+
+      public static void main(String[] args){
+
+
+                int arr[] = {19,4,7,5};
+
+                for(int i =0 ; i<arr.length; i++){
+
+                        System.out.println(arr[i]);
+                }
+
+		for(int x : arr) {
+		
+			System.out.println(x);
+		}
+
+        }
+
+}
+
+
