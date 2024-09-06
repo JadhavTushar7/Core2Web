70eda3595a3c68fcc5ea444c4b88c7475cc9ae02
tushar jadhav <132814429+JadhavTushar7@users.noreply.github.com>
2024-09-06 15:51:12 +0530

    Add files via upload

---
+++ ArrayDemo13.java
@@ -0,0 +1,24 @@
+class ArrayDemo13{
+
+	public static void main(String[] args){
+	
+		int arr1[]  = {10,20,30,40};
+		byte arr2[] = {1,2,3,4};
+		short arr3[] = {2,4,6,8};
+		boolean arr4[] = {true,false};
+
+		System.out.println(arr1);
+		System.out.println(arr2);
+		System.out.println(arr3);
+		System.out.println(arr4);
+
+
+
+
+	
+	
+	}
+
+
+
+}
