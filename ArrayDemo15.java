464ffe3ee21e07d2ab34303a31c1838eeef7b97e
tushar jadhav <132814429+JadhavTushar7@users.noreply.github.com>
2024-09-06 16:13:39 +0530

    Add files via upload

---
+++ ArrayDemo15.java
@@ -0,0 +1,15 @@
+class ArrayDemo15{
+
+	public static void main(String[] args){
+	
+
+		int arr[] = {19,4,7,5};
+
+		for(int i =0 ; i<arr.length; i++){
+		
+			System.out.println(arr[i]);
+		}
+	
+	}
+
+}
