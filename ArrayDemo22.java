cd7fc81e000bc24586e1d6b9eda31f0915251f7f
tushar jadhav <132814429+JadhavTushar7@users.noreply.github.com>
2024-09-07 11:59:18 +0530

    Add files via upload

---
+++ ArrayDemo22.java
@@ -0,0 +1,16 @@
+class ArrayDemo22 {
+
+
+	public static void main(String[] args){
+	
+		String friends[] = {"vaishnavi","tushar","swapnil","aditya"};
+
+		System.out.println(args[0]);
+		 System.out.println(args[1]);
+		  System.out.println(args[2]);
+	
+	
+	}
+
+
+}
