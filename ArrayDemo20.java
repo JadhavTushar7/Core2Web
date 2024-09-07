671632b3ee29748342b086a0a66f897e64bdd572
tushar jadhav <132814429+JadhavTushar7@users.noreply.github.com>
2024-09-07 11:42:42 +0530

    Add files via upload

---
+++ ArrayDemo20.java
@@ -0,0 +1,29 @@
+class ArrayDemo20{
+
+
+	static void fun(int xarr[]){
+	
+		for(int x : xarr){
+		
+		System.out.println(x);
+		}
+
+		xarr[0] = 50;
+	}
+
+
+	public static void main(String[] args) {
+	   
+
+		int arr[]  = {19,4,7};
+		 
+		 for(int x : arr) {
+		 
+			 System.out.println(x);
+		 }
+		
+		
+		}
+	
+		
+}
