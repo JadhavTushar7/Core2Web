593c43c38e817db5bb871c2157d8a8e4f7a0a741
tushar jadhav <132814429+JadhavTushar7@users.noreply.github.com>
2024-09-10 20:27:56 +0530

    Add files via upload

---
+++ ArrayDemo26TwoDArray.java
@@ -0,0 +1,12 @@
+class ArrayDemo26{
+
+	public static void main(String[] args) {
+	
+	
+		int arr[][] = new int[19][4];
+		int arr2[] =  new int[4];
+
+		System.out.println(arr.length);
+		System.out.println(arr2.length);
+	}
+}
