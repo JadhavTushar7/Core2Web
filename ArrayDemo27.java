98171beb8c00ecde85c574adc619347a73bde275
tushar jadhav <132814429+JadhavTushar7@users.noreply.github.com>
2024-09-10 20:35:37 +0530

    Add files via upload

---
+++ ArrayDemo27.java
@@ -0,0 +1,21 @@
+class ArrayDemo27 {
+
+	public static void main(String[] args) {
+	
+	
+		int arr[][] = new int[2][2];
+
+		arr[0][0] = 19;
+		 arr[0][1] = 4;
+		  arr[1][0] = 7;
+		   arr[1][1] = 5;
+
+
+		   System.out.println(arr[1][1]);
+		     System.out.println(arr[0]);
+		       System.out.println(arr[1]);
+		         System.out.println(arr);
+
+	}
+
+}
